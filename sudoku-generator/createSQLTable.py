a2c2ab2d98a13dad01bc513567d3bc27ee90cd16
taylorm-portfolio <153951705+taylorm-portfolio@users.noreply.github.com>
2024-02-28 19:19:16 -0600

    removed sudoku generator
    Removed this folder as it will be stored in another repository

--- sudoku-generator/createSQLTable.py
+++
@@ -1,4 +0,0 @@
-from connectDB import createTable
-
-# this calls a function that will create (or access, if already created, a database called "sodukostats.db"), and then create a table called "runs"
-createTable()
